92eb6c48432c6d4bbc76dd02fa8bb5cf939b9b36
Abner Rizzi <bacteria666@gmail.com>
2024-08-01 22:13:04 +0000

    test

--- tf-project/output.tf
+++ tf-project/output.tf
@@ -2,6 +2,6 @@
 #   value = aws_instance.instance[0].public_ip != "" ? "http://${aws_instance.instance[0].public_ip}" : "Instance not created"
 # }
 
-output "current_workspace_name" {
-  value = terraform.workspace
-}
+# output "current_workspace_name" {
+#   value = terraform.workspace
+# }
